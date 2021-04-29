e5f78de1da320ae8e60fe2c37e9f030f9f29b472
David Cook <davecook@hotmail.co.uk>
2021-04-29 16:04:16 -0500

    Update SpreadsheetManager.ts

--- classes/SpreadsheetManager.ts
+++ classes/SpreadsheetManager.ts
@@ -55,7 +55,7 @@ class SpreadsheetManager {
    * @param variable[][] or variable[] rows
    * @memberof SpreadsheetManager
    */
-  addNewRows(rows: Array<string | number | Date>[]) {
+  addNewRows(rows: Array<SpreadsheetManagerTypes.GenericRowValue>[]) {
     const { sheet } = this;
     if (!sheet) return;
     const lastRow = sheet.getLastRow();
@@ -71,15 +71,20 @@ class SpreadsheetManager {
   addNewRowsFromObjects(objects: SpreadsheetManagerTypes.GenericRowObject[]) {
     const { rowHeaders } = this;
     const newRows = objects.map((obj) => {
-      const newRow: Array<string | number | Date> = [];
+      const newRow: Array<SpreadsheetManagerTypes.GenericRowValue> = [];
       for (let header in rowHeaders) {
         const colIndex: number = rowHeaders[header];
-        newRow[colIndex] = obj[header] || "";
+        if (obj[header] === undefined) {
+          newRow[colIndex] = "";
+        } else {
+          newRow[colIndex] = obj[header];
+        }
       }
       return newRow;
     });
     this.addNewRows(newRows);
   }
+
 
   /**
    *
